3545cee87afe97d0fd2f287abbc0ea0956f3c9ac
Tracy Moscato <tracy@Tracys-MacBook-Air.local>
2013-09-16 22:04:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinkIt::Application.config.secret_token = '3f556fe9fc1a39cd2702b334b079e9c4fd16f0a0b8392805890b6cf78efc249d8d04209cc5f6873f61e55e690c54f9696856e722c4928218407e9142cc9c2e81'
